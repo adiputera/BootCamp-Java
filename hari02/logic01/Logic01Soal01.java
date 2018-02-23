7bd3d609783cf88b804e25306a2190051b6d76ac
Yusuf Fahmi Adiputera <yfa.adiputera@gmail.com>
2018-02-23 15:08:33 +0700

    hari2

---
+++ hari02/logic01/Logic01Soal01.java
@@ -0,0 +1,169 @@
+package hari02.logic01;
+
+import java.util.Scanner;
+
+import xsis.bc.util.PrintArray;
+
+public class Logic01Soal01 {
+	public static void main(String[] args) {
+		System.out.println("Masukkan Angka : ");
+		Scanner scn = new Scanner(System.in);
+		int n=scn.nextInt();
+		String[][] array2D = new String[n][n];
+		
+		// mengisi array
+		for (int i = 0; i < array2D.length; i++) {
+			for (int j = 0; j < array2D[i].length; j++) {
+				if(i==j) {
+					array2D[i][j]="*";
+				}
+				else {
+					array2D[i][j]="";
+				}
+			}
+		}
+		
+		//menampilkan array
+		for (int i = 0; i < array2D.length; i++) {
+			for (int j = 0; j < array2D[i].length; j++) {
+				System.out.print(array2D[i][j] + "\t");
+			}
+			
+			System.out.println("\n");
+		}
+		
+		System.out.println("ulang ke 1");
+		method1(n);
+		System.out.println("ulang ke 2");
+		method2(n);
+		System.out.println("ulang ke 3");
+		method3(n);
+		System.out.println("ulang ke 4");
+		method4(n);
+		System.out.println("ulang ke 5");
+		method5(n);
+		System.out.println("ulang ke 6");
+		method6(n);
+		System.out.println("ulang ke 7");
+		method7(n);
+		System.out.println("ulang ke 8");
+		method8(n);
+		System.out.println("ulang ke 9");
+		method9(n);
+		System.out.println("ulang ke 10");
+		method10(n);
+	}
+	
+	public static void method1(int n) {
+		String[][] data = new String[n][n];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if(i==j) {
+					data[i][j]="*";
+				}
+			}
+		}
+		PrintArray.array2D(data);
+	}
+	
+	public static void method2(int n) {
+		String[][] data = new String[n][n];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if(i==j) {
+					data[i][j]="*";
+				}
+			}
+		}
+		PrintArray.array2D(data);
+	}
+	
+	public static void method3(int n) {
+		String[][] data = new String[n][n];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if(i==j) {
+					data[i][j]="*";
+				}
+			}
+		}
+		PrintArray.array2D(data);
+	}
+	public static void method4(int n) {
+		String[][] data = new String[n][n];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if(i==j) {
+					data[i][j]="*";
+				}
+			}
+		}
+		PrintArray.array2D(data);
+	}
+	public static void method5(int n) {
+		String[][] data = new String[n][n];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if(i==j) {
+					data[i][j]="*";
+				}
+			}
+		}
+		PrintArray.array2D(data);
+	}
+	public static void method6(int n) {
+		String[][] data = new String[n][n];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if(i==j) {
+					data[i][j]="*";
+				}
+			}
+		}
+		PrintArray.array2D(data);
+	}
+	public static void method7(int n) {
+		String[][] data = new String[n][n];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if(i==j) {
+					data[i][j]="*";
+				}
+			}
+		}
+		PrintArray.array2D(data);
+	}
+	public static void method8(int n) {
+		String[][] data = new String[n][n];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if(i==j) {
+					data[i][j]="*";
+				}
+			}
+		}
+		PrintArray.array2D(data);
+	}
+	public static void method9(int n) {
+		String[][] data = new String[n][n];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if(i==j) {
+					data[i][j]="*";
+				}
+			}
+		}
+		PrintArray.array2D(data);
+	}
+	public static void method10(int n) {
+		String[][] data = new String[n][n];
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				if(i==j) {
+					data[i][j]="*";
+				}
+			}
+		}
+		PrintArray.array2D(data);
+	}
+}
